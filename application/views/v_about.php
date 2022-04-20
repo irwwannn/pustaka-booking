cd59370ead54a2b9fe2976a6728bc8a63e80ae27
irwwannn <irwanirwan1098@gmail.com>
2022-04-19 23:44:17 -0700

    menambahkan v_about.php,v_footer.php,v_header.php

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?PHP echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Irwan</li>
+        <li>Nama Belakang : irwan</li>
+     </ul>
+     <br>
+     <h4>Alamat</h4>
+     <ul type="none">
+         <li> Jalan jaya 25 No. 66 cengkareng barat</li>
+    </ul>
+ 
+     <h4>Tempat Lahir</h4>
+     <ul type="none">
+         <li>lampung bukit kemuning</li>
+     </ul>
+
+     <h4>Olah Raga Favorit</h4>
+     <ul type="square">
+         <li>football</li>
+         <li>Catur</li>
+    </ul>
+</section>
